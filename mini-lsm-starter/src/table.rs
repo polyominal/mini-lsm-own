68c65b326f802a79cd333dbc0676c84f6caca820
polyominal <79344013+polyominal@users.noreply.github.com>
2025-10-12 14:58:30 +0800

    cache

--- mini-lsm-starter/src/table.rs
+++ mini-lsm-starter/src/table.rs
@@ -237,11 +237,10 @@ impl SsTable {
 
     /// Read a block from disk, with block cache. (Day 4)
     pub fn read_block_cached(&self, block_idx: usize) -> Result<Arc<Block>> {
-        if let Some(ref block_cache) = self.block_cache {
-            let b = block_cache
+        if let Some(ref cache) = self.block_cache {
+            cache
                 .try_get_with((self.id, block_idx), || self.read_block(block_idx))
-                .map_err(|e| anyhow!("{e}"))?;
-            Ok(b)
+                .map_err(|e| anyhow!("{e}"))
         } else {
             self.read_block(block_idx)
         }
